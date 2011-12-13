54cbea863186c6c49f2621ccf2f8a32d5a3a5f88
Claudio Figueiredo <jcfigueiredo@gmail.com>
2011-12-13 15:05:42 -0200

    stting up info for CheeseShop

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "django-mercury",
     version = "0.1",
-    url = 'http://github.com/jacobian/django-shorturls',
+    url = 'https://github.com/jcfigueiredo/django-mercury',
     license = 'MIT',
+    long_description = read('README'),
     description = "Django support to Mercury Editor",
-    author = 'Claudio Figueiredo <jcfigueiredo@gmail.com>',
+    
+    author = 'Claudio Figueiredo',
+    author_email='jcfigueiredo@gmail.com',
+    
     packages = find_packages('src'),
     package_dir = {'': 'src'},
+    
     install_requires = ['setuptools'],
+    classifiers = [
+        'Development Status :: 1 - Planning',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
 )
